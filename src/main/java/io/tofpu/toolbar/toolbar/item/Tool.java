597db6c9e10aba3f757732d967da5655081fc4c1
Tofpu <tofpuu@gmail.com>
2023-12-15 15:57:22 +0200

    Add a couple of constructors for `Tool`

--- src/main/java/io/tofpu/toolbar/toolbar/item/Tool.java
+++ src/main/java/io/tofpu/toolbar/toolbar/item/Tool.java
@@ -4,6 +4,8 @@ import io.tofpu.toolbar.toolbar.Toolbar;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.StringJoiner;
+
 public class Tool {
     private final String itemIdentifier;
     private final ItemStack item;
@@ -20,6 +22,14 @@ public class Tool {
 
     public Tool(final String id, final ItemStack item, final ToolAction action) {
         this(id, item, -1, action);
+    }
+
+    public Tool(final String id, final ItemStack item, final int index) {
+        this(id, item, index, ToolAction.empty());
+    }
+
+    public Tool(final String id, final ItemStack item) {
+        this(id, item, -1);
     }
 
     public String getItemIdentifier() {
@@ -44,5 +54,15 @@ public class Tool {
             return;
         }
         action.trigger(toolbar, event);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Tool.class.getSimpleName() + "[", "]")
+                .add("itemIdentifier='" + itemIdentifier + "'")
+                .add("item=" + item)
+                .add("index=" + index)
+                .add("action=" + action)
+                .toString();
     }
 }

77a9cf73e55e50ea6bc266b6dcffde34b3ce04ee
Tofpu <tofpuu@gmail.com>
2023-12-15 15:56:54 +0200

    Add `ToolAction#empty()`

--- src/main/java/io/tofpu/toolbar/toolbar/item/ToolAction.java
+++ src/main/java/io/tofpu/toolbar/toolbar/item/ToolAction.java
@@ -5,5 +5,9 @@ import org.bukkit.event.player.PlayerInteractEvent;
 
 @FunctionalInterface
 public interface ToolAction {
+    static ToolAction empty() {
+        return (toolbar, event) -> {};
+    }
+
     void trigger(final Toolbar toolbar, final PlayerInteractEvent event);
 }
